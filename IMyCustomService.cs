f27ca26eea503b414eed4efc15b8ef9f92affac7
Ershad Raoufi- ارشاد رئوفی <waveershad21@gmail.com>
2022-07-30 12:30:56 +0430

    Add files via upload
    Add Custom Service Just for Test

---
+++ IMyCustomService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services
+{
+    public interface IMyCustomService: ICustomService
+    {
+        int ok();
+    }
+}
---
+++ MyCustomService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services
+{
+    public class MyCustomService : IMyCustomService
+    {
+        public InjectType Inject { get; set; }
+        public MyCustomService()
+        {
+            Inject = InjectType.Scopped;
+        }
+        public int ok()
+        {
+            return 10;
+        }
+    }
+}
